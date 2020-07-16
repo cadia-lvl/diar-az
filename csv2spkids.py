4755e5003961f730d31d99b1a7a362f2f5ff17de
Arnar Freyr Kristinsson <arnar11@hotmail.com>
2020-07-16 15:09:59 +0000

    Other tasks - task 3 - Manual process - correcting spelling mistakes

--- csv2spkids.py
+++ csv2spkids.py
@@ -7,58 +7,112 @@
 
 from Levenshtein import distance
 from collections import Counter
-import numpy as np
 
-
+#creates an id
 def creating_id(spk_type, spkr_number):
         spk_id = '{}{:04}'.format(spk_type, spkr_number)
         return spk_id
 
+#Sometimes, as in the case when the names are very similar, 
+# the script will correct spelling mistakes when it isn't supposed to do so.
+#So manual verification is needed, but only for these lines 
+# that actually may have spelling errors
+def manual_correction(old_name, new_name):
+    if(old_name == new_name):
+        return new_name
+
+    print("Type in enter if the correction is correct, otherwise type in the correct name")
+    name = input("{} will be corrected to: {} ".format(old_name, new_name))
+    if(name == ""):
+        return new_name
+    else:
+        return name
+
+#Corrects a name to have uppercase or lower case where appropriate
+def name_to_capital(name):
+    full_name = name.split()
+    index = 0
+    for name in full_name:
+        full_name[index] = (name[0].upper()) + (name[1:].lower())
+        index = index + 1
+    return " ".join(full_name)
+
 # Corrects spelling mistakes depending on which possible misspelled name appears the most often
-def correct_spelling_mistakes(row, contents):
+def correct_spelling_mistakes(row, contents, corrected):
     name = row[1][2]
+    name = name_to_capital(name)
     tmp_names = {}
     tmp_names[row[0]] = name
+    contents.sort()
     for other_name in enumerate(contents):
         if(distance(name, other_name[1][2]) >= 0 and distance(name, other_name[1][2]) <= 3):
             tmp_names[other_name[0]] = other_name[1][2]
-
     most_common = Counter(tmp_names.values()).most_common(1)[0]
-    for line, name in tmp_names.items():
-        row[1][2] = most_common[0]
-    return row
+    most_common = name_to_capital(most_common[0])
+    
+    if(name not in corrected):
+        row[1][2] = manual_correction(name, most_common)
+    else:
+        row[1][2] = most_common
+    return (name, row[1][2], row)
+#Adds known speakers corrections to a dictonary so it's not needed to correct it manually many times
+def correct_known_speakers(row, contents):
+    corrected = {}
+    correction = correct_spelling_mistakes(row, contents, corrected)
+    old_name =  correction[0]
+    new_name = correction[1]
+    corrected[old_name] = new_name
 
-
-def main():
+def main(name_file, correct_spelling):
     import csv
     spk_ids = {}
-    with open('../reco2spk_num2spk_name.csv') as \
+    with open(name_file) as \
         csvfile, open('../reco2spk_num2spk_label.csv', 'w') as spk_label, open('../reco2spk_num2spk_info.csv', 'w') as spk_info:
         spkreader = csv.reader(csvfile, delimiter=',')
         known_spkr_number = 1
         unknown_spkr_number = 1
-        spkr_names = []
         contents = []
+        episodes = []
+        episode = None
+        unknown = None
+        unk_num = 1
 
         for row in spkreader:
             contents.append(row)
 
         for row in enumerate(contents):
-            correct_spelling_mistakes(row, contents)
-            row = row[1]
-            if(row[2].split()[0] == "Unknown"):
-                print(row[0].split("-")[1], row[1], row[2],creating_id("UNK", unknown_spkr_number), sep=',', file=spk_info)
-                print(row[0].split("-")[1], row[1],creating_id("UNK", unknown_spkr_number), sep=',', file=spk_label)
-                unknown_spkr_number = unknown_spkr_number + 1
-            else: 
-                if(row[2] not in spk_ids):
-                    spk_ids[row[2]] = creating_id("SPK", known_spkr_number)
+            name = row[1][2]
+            recording_id = row[1][0]
+            if(name.split()[0] != "Unknown"):
+                if(correct_spelling == "True"):
+                    correct_known_speakers(row, contents) 
+                if(name not in spk_ids):
+                    spk_ids[name] = creating_id("SPK", known_spkr_number)
                     known_spkr_number = known_spkr_number + 1
-                print(row[0].split("-")[1], row[1],spk_ids[row[2]], sep=',', file=spk_label)
-                print(row[0].split("-")[1], row[1], row[2],spk_ids[row[2]], sep=',', file=spk_info)
+                print(recording_id.split("-")[1], name,spk_ids[name], sep=',', file=spk_label)
+                print(recording_id.split("-")[1], name, name,spk_ids[name], sep=',', file=spk_info)
+            else:
+                episode = recording_id.split("-")[1]
+                episodes.sort()
+                if(episode in episodes):
+                    unknown_spkr_number = unknown_spkr_number + 1
 
-    print("Spelling mistakes have been corrected.")
+                else:
+                    episodes.append(episode)
+                    unknown_spkr_number = 1
+                unknown = "Unknown {}".format(unknown_spkr_number)
+                print(recording_id.split("-")[1], unknown, creating_id("UNK", unk_num ), sep=',', file=spk_info)
+                print(recording_id.split("-")[1], unknown,creating_id("UNK", unknown_spkr_number), sep=',', file=spk_label)
+                unk_num = unk_num + 1
+           
+    if(correct_spelling == "True"):
+        print("Spelling mistakes have been corrected.")
     print("The speaker labels have been created.")
 
 if __name__ == '__main__':
-    main()
+    import argparse
+    parser = argparse.ArgumentParser(description='The arguments need to be passed in.')
+    parser.add_argument('--file', default= "../reco2spk_num2spk_name.csv", help='the path to the file')
+    parser.add_argument('--correct_spelling', default="True", help='correct spelling mistakes on or off (True on False off)')
+    args = parser.parse_args()
+    main(args.file, args.correct_spelling)
