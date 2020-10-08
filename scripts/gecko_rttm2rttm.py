a8bc8d8ee1e1ae87137600ad117f9ff501bcde77
judyfong <judyfong@users.noreply.github.com>
2020-10-08 11:47:13 +0000

    Remove trailing spaces and fix up the comments

--- scripts/gecko_rttm2rttm.py
+++ scripts/gecko_rttm2rttm.py
@@ -1,23 +1,26 @@
 # Authors: Reykjavik University (Judy Fong <judyfong@ru.is>) and (Arnar Freyr Kristinsson <arnark17@ru.is> )
-# Description: Convert gecko rttm files to also have recordingids in the first <NA>,
-# adds audio filenames to rttm files as the second field,
-# remove [] stuff (foreign, noise, music) from rttm files where there is a [something]+number,
-#renames the rttm/json/srt files to just the audio filename 
-# and calls the create_segments_and_text.py
-#All fields are optional: --rrtm, --srt or --subtitle-file
-#If none arguments are provided the script will only rename the corresponding files if they exist and update the readme file
+# Description: This script does several things.
+# 1. Convert gecko rttm files to also have recordingids in the first <NA>,
+#    adds audio filenames to rttm files as the second field,
+# 2. remove [] stuff (foreign, noise, music) from rttm files where there is a
+#    [something]+number,
+# 3. renames the rttm/json/srt files to just the audio filename
+# 4. and calls the create_segments_and_text.py
+# All fields are optional: --rrtm, --srt or --subtitle-file
+# If no arguments are provided the script will only rename the corresponding
+# files if they exist and update the readme file
 
-from decimal import * 
+from decimal import *
 import create_segments_and_text
 
-#removes [something]+number (speaker number) and number+[something] - rttm files
+# Removes [something]+number (speaker number) and number+[something] - rttm files
 def rm_brckts_spker_rttm(line, audiofilename):
-    
+
     if(line != '\n'):
         spkridOrBracketStuff = line.split()[7]
         endbrackpos = line.find("]")
         bgnbrackpos = line.find("[")
-    
+
         if "]+" in spkridOrBracketStuff:
             removed = line.replace ( line[ bgnbrackpos : endbrackpos+2 ], "")
             return removed
@@ -28,12 +31,12 @@ def rm_brckts_spker_rttm(line, audiofilename):
         else:
             return line
 
-#Convert timestamps to seconds and partial seconds ss.ff
+# Convert timestamps to seconds and partial seconds ss.ff
 def cnvrt_hh_mm_sec(hh_mm_ss):
     hh, m, s = hh_mm_ss.split(':')
     return ( int(hh) * 3600 ) + ( int(m) * 60) + Decimal(s.replace(",","."))
 
-#Checks if a string is a timestamp
+# Checks if a string is a timestamp
 def is_srt_tmstmp(tmstamp):
     mintmstamplen = 8 # 00:00:00 (hh:mm:ss) - minimum timestamp length
     if (len (tmstamp) >= mintmstamplen):
@@ -42,8 +45,8 @@ def is_srt_tmstmp(tmstamp):
                     and tmstamp[9:].isnumeric() ):
             return True
     return False
-    
-#checks if there is some speech in the rttm file at specific segment 
+
+# Check if there is some speech in the rttm file at specific segment
 def is_speech_rttm(srt_line, rttm_lines):
     if(tmstmp_scnds(srt_line) != []):
         srt_range = tmstmp_scnds(srt_line)
@@ -54,19 +57,20 @@ def is_speech_rttm(srt_line, rttm_lines):
                 if(rttm_spkr.isnumeric()):
                     return False
     return True
-#Returns the timestamps in seconds to compare the srt file to the rttm file to remove the correct segments with no speech in it.
+# Returns the timestamps in seconds to compare the srt file to the rttm file to
+# remove the correct segments with no speech in it.
 def tmstmp_scnds(line):
       fstcol = line.split("\n")[0].split(' ')[0]
       lstcol = line.split("\n")[0].split(' ')[-1]
       arrow = "-->"
-      #Find the correct line (a line that has a timestamp)
+      # Find the correct line (a line that has a timestamp)
       if( is_srt_tmstmp(fstcol) and is_srt_tmstmp(lstcol)):
         expln = fstcol + " " + arrow + " " + lstcol
         if(expln == line[:-1]):
             return [ cnvrt_hh_mm_sec(fstcol), cnvrt_hh_mm_sec(lstcol) ]
       return []
 
-#Gets the audio filename
+# Gets the audio filename
 # NOTE: this function fails if the files were in an windows archive and
 # unzipped to a linux machine without specifying the encoding of the original
 # archive
@@ -83,7 +87,7 @@ def get_audio_filename(filename, os):
     else:
         return filename+ext
 
-#renames files given a list contents of a directory and a file type
+# Renames files given a list contents of a directory and a file type
 def rename(dircontents, dirname, os):
     for filename in dircontents:
         audiofilename = get_audio_filename(filename, os)
@@ -91,7 +95,7 @@ def rename(dircontents, dirname, os):
         (fl, ext) = os.path.splitext(filename)
         print("The file {} has been renamed to {}".format(filename, audiofilename))
 
-#Renames Json, Rttm and srt files
+# Renames Json, Rttm and srt files
 def rnm_json_rttm_srt(os):
     json = 'json'
     rttm = 'rttm'
@@ -109,7 +113,7 @@ def rnm_json_rttm_srt(os):
         srt_files = os.listdir(segments)
         rename(srt_files, segments, os)
 
-#Checks the given arguments and calls the corresponding function
+# Checks the given arguments and calls the corresponding function
 def checkArguments(args):
     if args.only_csv == 'True':
         create_csv(args.create_csv)
@@ -120,8 +124,8 @@ def checkArguments(args):
         if args.update_ruv_di_readme_off == 'false':
             update_ruv_di_readme(args.ruv_di_readme, "Statistics", args.statistics)
         exit(0)
-        
-#Trims the srt file - removes segments that don't have any speech
+
+# Trims the srt file - removes segments that don't have any speech
 def trim_srt(gecko_srt, srt_folder, rttm_lines, os):
     base = os.path.basename(gecko_srt)
     segment_id = 0
@@ -130,17 +134,17 @@ def trim_srt(gecko_srt, srt_folder, rttm_lines, os):
         os.mkdir(srt_folder)
     with open(srt_folder+gecko_srt, 'r') as gecko_srt_file:
         for line in gecko_srt_file:
-                #For the segment id
+                # For the segment id
                 if(line.rstrip().isalnum()):
                     segment_id = segment_id + 1
                 if not is_speech_rttm(line, rttm_lines):
                     segment = segment + str(segment_id) + "\n" + line + "\n"
-  
+
     with open(srt_folder+gecko_srt, 'w') as srt_file:
         print(segment, end='\n', file=srt_file)
     print("The file {} has been trimmed".format(base))
 
-#Removes []+number stuff and number+[] stuff
+# Removes []+number stuff and number+[] stuff
 def trim_rttm(gecko_rttm, rttm_folder, os):
     base = os.path.basename(gecko_rttm)
     contents = ""
@@ -161,9 +165,9 @@ def trim_rttm(gecko_rttm, rttm_folder, os):
     print("The file {} has been trimmed".format(base))
     return contents.split('\n')[:-1]
 
-#Creates the csv file
+# Creates the csv file
 def create_csv(csv_filename):
-    import csv2spkids #The csv script used to create the csv file
+    import csv2spkids # The csv script used to create the csv file
     csv2spkids.main(csv_filename, "True")
     print("CSV file have been created")
 
@@ -173,7 +177,7 @@ def total_speech_time():
     segment_time = 0
     segment_folder = "./segments"
     segments_files = os.listdir(segment_folder)
-    segment_time = [] #For sorting segment time for correct subtraction
+    segment_time = [] # For sorting segment time for correct subtraction
     segment_cnt = 0
     fstcol = None
     lstcol = None
@@ -185,21 +189,24 @@ def total_speech_time():
                 lstcol = line.split("\n")[0].split(' ')[-1]
                 if(is_srt_tmstmp(fstcol) and is_srt_tmstmp(lstcol)):
                     segment_time = cnvrt_hh_mm_sec(lstcol) - cnvrt_hh_mm_sec(fstcol)
-                    # Some .srt files may contain invalid srt line(s) that affect the calculations, 
-                    # so some minimal .srt verfication is done here. 
-                    # Gecko should fix this when .srt file is saved again, including the segment id's. 
-                    # To avoid this in the future a little bit of verification must take place.
+                    # Some .srt files may contain invalid srt line(s) that
+                    # affect the calculations,
+                    # so some minimal .srt verfication is done here.
+                    # Gecko should fix this when .srt file is saved again,
+                    # including the segment id's.
+                    # To avoid this in the future a little bit of verification
+                    # must take place.
                     segment_cnt = segment_cnt + 1
                     if(segment_time < 0):
                         print("Segment failure")
                         print(line)
                         print("The segment above, segment id: {} in the file {} needs to be fixed".format(segment_cnt, segment_file))
-                        exit(0) #Obviously something wrong so won't continue until the file is fixed manually
+                        exit(0) # Obviously something wrong so won't continue until the file is fixed manually
                     total = total + segment_time
             segment_cnt = 0
     return total
 
-#Creates a string to print in the Readme for statistics
+# Creates a string to print in the Readme for statistics
 def statistics_string(total_speakers, total_time, ided_speakers, unknown_speakers):
     statistics = "\n----------\n"
     statistics_lines = None
@@ -207,11 +214,12 @@ def statistics_string(total_speakers, total_time, ided_speakers, unknown_speaker
     hours = round(total_mins / 60)
     minutes = round(total_mins - (60*hours))
     statistics = statistics + "{} minutes ({} hrs {} mins) of speech\n{} ided speakers\n".format(total_mins, hours, minutes, ided_speakers)
-    statistics = statistics + "{} unknown speakers\n".format(unknown_speakers) 
+    statistics = statistics + "{} unknown speakers\n".format(unknown_speakers)
     statistics = statistics + "{} total speakers\n".format(total_speakers)
     return statistics
 
-#Creates the statistics from the Info CSV file and from the segments files in the segments folder
+# Creates the statistics from the Info CSV file and from the segments files in
+# the segments folder
 def create_statistics(csv_info_file):
     ided_speakers = 0
     unknown_speakers = 0
@@ -229,13 +237,15 @@ def create_statistics(csv_info_file):
 
     ided_speakers = len(speaker_names)
     total_speakers = ided_speakers + unknown_speakers
-    
+
     stats = statistics_string(total_speakers, total_time, ided_speakers, unknown_speakers)
     return stats
 
-#Auto replaces statistics given there is a line that has some string that indicates the correct place in the file
-#Only one occourence of any name should be taken into the account so for people who share exactly the same name, 
-#one of them will only be considered.
+# Auto replaces statistics given there is a line that has some string that
+# indicates the correct place in the file
+# Only one occourence of any name should be taken into the account so for
+# people who share exactly the same name,
+# only one of them will be considered.
 def update_ruv_di_readme(ruv_di_readme, statistics_indicator, csv_info_file):
     statistics = create_statistics(csv_info_file)
     statistics_line_count = 0
@@ -244,10 +254,12 @@ def update_ruv_di_readme(ruv_di_readme, statistics_indicator, csv_info_file):
         for line in readme_file_contents:
             if(line.rstrip() == statistics_indicator):
                 readme_contents = readme_contents + statistics_indicator + statistics
-                #How many lines to skip in the original file - create statistics is the string for writing
-                #in the readme file
-                # -2 because of new line in the end - don't want to skip new line
-                statistics_line_count = len(create_statistics(csv_info_file).split('\n'))-2 
+                # How many lines to skip in the original file - create
+                # statistics is the string for writing
+                # in the readme file
+                # -2 because of new line in the end - don't want to skip new
+                # line
+                statistics_line_count = len(create_statistics(csv_info_file).split('\n'))-2
             elif(statistics_line_count == 0):
                 readme_contents = readme_contents + line
             else:
@@ -258,11 +270,11 @@ def update_ruv_di_readme(ruv_di_readme, statistics_indicator, csv_info_file):
 
     print("Statistics have been updated")
 
-#Feeds the create_segments script of srt files
+# Feeds the create_segments script of srt files
 def create_segments(os):
     srt_folder = 'segments/'
     srt_files = os.listdir(srt_folder)
-    
+
     for filename in srt_files:
         create_segments_and_text.main("{}/{}".format(srt_folder, filename))
 
@@ -272,15 +284,15 @@ def main():
     srt_folder = 'segments/'
     rttm_folder = 'rttm/'
     rnm_json_rttm_srt(os)
-    #for each file in srt folder and for each file in rttm folder
-    srt_files = sorted(os.listdir(srt_folder)) 
+    # for each file in srt folder and for each file in rttm folder
+    srt_files = sorted(os.listdir(srt_folder))
     rttm_files = sorted(os.listdir(rttm_folder))
-   
-    #Taking usage of the fact there is a rttm file for each srt file
+
+    # Taking usage of the fact there is a rttm file for each srt file
     for filename_enum in enumerate(rttm_files):
         line_number = filename_enum[0]
         rttm_file = filename_enum[1]
-        srt_file = srt_files[line_number] #same line number as in rttm file as there is a .rttm file for each .srt file
+        srt_file = srt_files[line_number] # same line number as in rttm file as there is a .rttm file for each .srt file
         rttm_base = os.path.basename(rttm_file)
         srt_base = os.path.basename(srt_file)
         rttm_lines = trim_rttm(rttm_base, rttm_folder, os)
